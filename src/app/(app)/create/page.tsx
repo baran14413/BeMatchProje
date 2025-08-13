19acaccdedab277c470950f93285b4df767aa010
Wilmar Nickel <baranguler.65091@gmail.com>
2025-08-13 23:29:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/create/page.tsx
+++ src/app/(app)/create/page.tsx
@@ -14,6 +14,7 @@ import {
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
+import { Label } from '@/components/ui/label';
 import {
   ChevronLeft,
   ChevronRight,
