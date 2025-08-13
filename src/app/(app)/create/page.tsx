0f53616157c1bfe4548244a24cba8f9efbea7642
Wilmar Nickel <baranguler.65091@gmail.com>
2025-08-13 22:09:21 +0000

    Kırpma sistemide çalışmıyor istediğim yeri kıramıyorum

--- src/app/(app)/create/page.tsx
+++ src/app/(app)/create/page.tsx
@@ -67,33 +67,37 @@ function canvasPreview(
   canvas: HTMLCanvasElement,
   crop: PixelCrop
 ) {
-  const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d')
 
   if (!ctx) {
-    throw new Error('No 2d context');
+    throw new Error('No 2d context')
   }
 
-  const scaleX = image.naturalWidth / image.width;
-  const scaleY = image.naturalHeight / image.height;
+  const scaleX = image.naturalWidth / image.width
+  const scaleY = image.naturalHeight / image.height
+  const pixelRatio = window.devicePixelRatio
   
-  const pixelRatio = window.devicePixelRatio;
-  canvas.width = Math.floor(crop.width * scaleX * pixelRatio);
-  canvas.height = Math.floor(crop.height * scaleY * pixelRatio);
+  canvas.width = Math.floor(crop.width * scaleX * pixelRatio)
+  canvas.height = Math.floor(crop.height * scaleY * pixelRatio)
 
-  ctx.scale(pixelRatio, pixelRatio);
-  ctx.imageSmoothingQuality = 'high';
+  ctx.scale(pixelRatio, pixelRatio)
+  ctx.imageSmoothingQuality = 'high'
 
-  const cropX = crop.x * scaleX;
-  const cropY = crop.y * scaleY;
+  const cropX = crop.x * scaleX
+  const cropY = crop.y * scaleY
 
-  const centerX = image.naturalWidth / 2;
-  const centerY = image.naturalHeight / 2;
+  const centerX = image.naturalWidth / 2
+  const centerY = image.naturalHeight / 2
 
-  ctx.save();
-  
-  ctx.translate(-cropX, -cropY);
-  ctx.translate(centerX, centerY);
-  ctx.translate(-centerX, -centerY);
+  ctx.save()
+
+  // 5) Move the crop origin to the canvas origin (0,0)
+  ctx.translate(-cropX, -cropY)
+  // 4) Move the origin to the center of the original position
+  ctx.translate(centerX, centerY)
+  // 2) Scale the image
+  // 1) Move the center of the image to the origin (0,0)
+  ctx.translate(-centerX, -centerY)
   ctx.drawImage(
     image,
     0,
@@ -103,10 +107,10 @@ function canvasPreview(
     0,
     0,
     image.naturalWidth,
-    image.naturalHeight
-  );
+    image.naturalHeight,
+  )
 
-  ctx.restore();
+  ctx.restore()
 }
 
 
