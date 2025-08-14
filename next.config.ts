ead29904a32d1a37e4b37f567d9a8a8253728203
Wilmar Nickel <baranguler.65091@gmail.com>
2025-08-14 09:53:53 +0000

    Yani yapacağımız tüm değiştirmeler pwada da sorunsuz görünsün

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ import withPWAInit from '@ducanh2912/next-pwa';
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "offlineCache",
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig: NextConfig = {
